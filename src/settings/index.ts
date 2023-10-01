ef17304846bb38a77458fd2d3f9f1fa1d276cdf7
hkgnp <github.com/hkgnp>
2023-10-01 21:50:00 +0800

    fix: duplicate settings

--- src/settings/index.ts
+++ src/settings/index.ts
@@ -76,14 +76,6 @@ export const callSettings = async (projects: string[], labels: string[]) => {
         "If true, tasks will be added under a parent block with their project name.",
     },
     {
-      key: "retrieveAppendCreationDateTime",
-      type: "boolean",
-      default: false,
-      title: "Append Creation Date and Time",
-      description:
-        "If set to true, all retrieved tasks will have their creation date and time appended.",
-    },
-    {
       key: "retrieveAppendTodo",
       type: "boolean",
       default: true,
